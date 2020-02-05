25eb39f5b1dac79f08c5ebac7588c043b2213410
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2020-02-04 22:27:06 -0800

    Add newline

--- samples/git_version.py
+++ samples/git_version.py
@@ -5,7 +5,8 @@ def version():
     return subprocess.check_output(["git", "describe", "--tags"]).strip()
 
 def write_version(output_path):
-    output = """#define VERSION \"{0}\"""".format(version())
+    output = """#define VERSION \"{0}\"
+""".format(version())
     with open(output_path, "w") as f:
         f.write(output)
 
